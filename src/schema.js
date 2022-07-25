824bf09839c3e5a73ae78741292c3860c377a0bc
adejbakh <adejbakh@student.42.fr>
2022-07-25 15:53:47 +0200

    added vscode comments

--- src/schema.js
+++ src/schema.js
@@ -1,23 +1,42 @@
-import { parse }  from "parse5"
+// import { parse }  from "parse5"
 
-function parseSchema(htmlString) {
-    const dom = parse(htmlString)
-    JSONSchema(htmlString)
-    debugger
+/**
+ * Find and parse all schema in a html file
+ * Remove duplicate if any are present.
+ * @param {string} html  raw html as you find it on a webpage
+ * @returns {Array<Object>} array of schema
+ */
+function parseSchema(html) {
+    // const dom = parse(html)
+    const arrayOfJson = JSONSchema(html)
+
+    removeDuplicateInfo(arrayOfJson)
+    return 
 }
 
-function JSONSchema(htmlString){
+/**
+ * Find and parse json schema in a html file
+ * @param {string} html raw html as you find it on a webpage
+ * @returns {Array<Object>} array of schema
+ */
+function JSONSchema(html){
     const jsonRegex = /type="application\/ld\+json"\s*>(.*?)<\/script>/gsi
 
     let JSONArray = []
-    const results = [...htmlString.matchAll(jsonRegex)]
+    const results = [...html.matchAll(jsonRegex)]
     results.forEach(result => {
         JSONArray.push(JSON.parse(result[1]))
     })
-    // regex for <script type="application/ld+json">(.*?)<\/script>
-
     return JSONArray
 }
 
+/**
+ * Remove duplicate if any are present.
+ * @param {Array<Object>} schemas  array of schema
+ * @returns {Array<Object>} array of schema
+ */
+function removeDuplicateInfo(schemas){
 
-export  { parseSchema }
+}
+
+export  { parseSchema, JSONSchema }
