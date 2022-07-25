78373b2b50d0c79fcb3acd880827d11ee792f9d5
adejbakh <adejbakh@student.42.fr>
2022-07-25 16:20:48 +0200

    proto removeDuplicateInfo

--- src/schema.js
+++ src/schema.js
@@ -1,4 +1,5 @@
 // import { parse }  from "parse5"
+import {isDeepStrictEqual} from "util"
 
 /**
  * Find and parse all schema in a html file
@@ -7,11 +8,11 @@
  * @returns {Array<Object>} array of schema
  */
 function parseSchema(html) {
-    // const dom = parse(html)
-    const arrayOfJson = JSONSchema(html)
+  // const dom = parse(html)
+  const arrayOfJson = JSONSchema(html)
 
-    removeDuplicateInfo(arrayOfJson)
-    return 
+  removeDuplicateInfo(arrayOfJson)
+  return 
 }
 
 /**
@@ -20,23 +21,32 @@ function parseSchema(html) {
  * @returns {Array<Object>} array of schema
  */
 function JSONSchema(html){
-    const jsonRegex = /type="application\/ld\+json"\s*>(.*?)<\/script>/gsi
+  const jsonRegex = /type="application\/ld\+json"\s*>(.*?)<\/script>/gsi
 
-    let JSONArray = []
-    const results = [...html.matchAll(jsonRegex)]
-    results.forEach(result => {
-        JSONArray.push(JSON.parse(result[1]))
-    })
-    return JSONArray
+  let JSONArray = []
+  const results = [...html.matchAll(jsonRegex)]
+  results.forEach(result => {
+    JSONArray.push(JSON.parse(result[1]))
+  })
+  return JSONArray
 }
 
 /**
  * Remove duplicate if any are present.
+ * This function will need to be reworked later to catch small difference
  * @param {Array<Object>} schemas  array of schema
  * @returns {Array<Object>} array of schema
  */
 function removeDuplicateInfo(schemas){
+  const maxlen = schemas.length
+  schemas.forEach((schema, index) => {
+    if(index < maxlen){
+      if (isDeepStrictEqual(schema, schemas[index+1]))
+        schemas.splice(index+1,1) // remove duplicate
 
+    }
+  })
+  return schemas
 }
 
 export  { parseSchema, JSONSchema }
